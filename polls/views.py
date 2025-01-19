a64c98c1472e877e0eccb3742412dee59d5a85c3
corea2uca <corea2u3@naver.com>
2025-01-19 17:46:07 +0900

    css

--- polls/views.py
+++ polls/views.py
@@ -7,33 +7,24 @@ from .models import Choice, Question
 from django.http import Http404
 from django.views import generic
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     context = {"latest_question_list": latest_question_list}
-#     return render(request, "polls/index.html", context)    
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
-    def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
-        
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/detail.html", {"question": question})
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/results.html", {"question": question})
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    # return HttpResponse("vote: %s" % question_id)
+    # return HttpResponse("views.vote: %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
@@ -46,7 +37,9 @@ def vote(request, question_id):
         )
     else:
         selected_choice.votes = F("votes") + 1
+        # selected_choice.votes += 1
         selected_choice.save()
-        # POST 성공후, HttpResponseRedirect반환, 뒤로버튼 눌렀을때 2번 게시되는것 방지
-        # reverses(앱:url)는 urls.py - name="results" 참조 (<int:question_id>/results/)
+        # 결과 페이지 refresh 하더라도, 다시 폼 데이터가 전송x, 
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        # 결과 페이지 refresh 하면, 재전송
+        # return render(request, "polls/results.html", {"question": question})
